bb825bf96d709dc9010a79cf314ed0bb2d537429
llaksa <irvin.jair.pg@gmail.com>
2018-11-28 19:23:56 -0800

    improve mysql excercise

--- databases/mysql-excercise.sql
+++ databases/mysql-excercise.sql
@@ -15,7 +15,7 @@ CREATE TABLE IF NOT EXISTS books (
        book_id INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
        author_id INTEGER UNSIGNED, -- referencia a la tabla "authors"
        title VARCHAR(100) NOT NULL,
-       year INTEGER UNSIGNED NOT NULL DEFAULT 1900,
+       `year` INTEGER UNSIGNED NOT NULL DEFAULT 1900,
        languaje VARCHAR(2) NOT NULL DEFAULT 'es' COMMENT 'iso 639-1 languaje',
        cover_url VARCHAR(500),
        price DOUBLE(6,2) NOT NULL DEFAULT 10.0, -- seis espacios en total, dos dedicados a decimales
@@ -53,3 +53,16 @@ CREATE TABLE IF NOT EXISTS books (
        `copies` INTEGER UNSIGNED NOT NULL DEFAULT 1,
        `description` TEXT
 );
+-- Para el ejercicio, NO USAR la tabla books con nombres de columnas entre ``
+
+CREATE TABLE clients (
+       client_id INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+       `name` VARCHAR(50) NOT NULL,
+       email VARCHAR(100) NOT NULL UNIQUE,
+       birthdate DATETIME, -- Óptimo para guardar fechas, tiempos pasados
+       gender ENUM('M', 'F', 'ND') NOT NULL,
+       active TINYINT(1) NOT NULL DEFAULT 1,
+       created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP -- 'yyyy-mm-dd hh:mm:ss' -- Óptimo para guardar momentos,
+       updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
