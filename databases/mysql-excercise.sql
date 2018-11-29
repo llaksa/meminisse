52cd05076f67fef99070cbf531a3049b2e494068
llaksa <irvin.jair.pg@gmail.com>
2018-11-28 19:11:10 -0800

    improve mysql excercise

--- databases/mysql-excercise.sql
+++ databases/mysql-excercise.sql
@@ -38,3 +38,18 @@ DESCRIBE books;
 SHOW FULL COLUMNS FROM books:
 
 DROP authors; -- borra la tabla "authors" definitivamente, no hay vuelta atrás
+
+-- A veces los nombres de las columnas pueden ser palabras reservadas de algún lenguaje de programación
+-- Entonces una opción es colocar esos nombres entre ``, como se muestra a continuación
+CREATE TABLE IF NOT EXISTS books (
+       `book_id` INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+       `author_id` INTEGER UNSIGNED, -- referencia a la tabla "authors"
+       `title` VARCHAR(100) NOT NULL,
+       `year` INTEGER UNSIGNED NOT NULL DEFAULT 1900,
+       `languaje` VARCHAR(2) NOT NULL DEFAULT 'es' COMMENT 'iso 639-1 languaje',
+       `cover_url` VARCHAR(500),
+       `price` DOUBLE(6,2) NOT NULL DEFAULT 10.0, -- seis espacios en total, dos dedicados a decimales
+       `sellable` TINYINT(1) DEFAULT 1, -- banderas: true or false
+       `copies` INTEGER UNSIGNED NOT NULL DEFAULT 1,
+       `description` TEXT
+);
